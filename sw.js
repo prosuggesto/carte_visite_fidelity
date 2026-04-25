5c7770def237f8b0c4b91e42347c830a87b38843
Diego Lamperim <lamperim.diego.fidelity.es@gmail.com>
2026-04-25 10:33:24 +0200

    fix(ui): remove back accent line to uncross text and fix mobile sticky hover double tap

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Caches all assets for offline access
    ============================================ */
 
-const CACHE_NAME = 'contact-card-v8';
+const CACHE_NAME = 'contact-card-v9';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
